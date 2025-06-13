1e9fd83d34bbf038bdbf9b134719caec1dc80a27
Takexin <ftrangel07@gmail.com>
2025-06-13 16:02:31 -0300

    testing

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from spherov2.types import Color
 toy = scanner.find_toy()
 
 with SpheroEduAPI(toy) as droid:
+    print("toy found")
     droid.set_main_led(Color(0,0,255))
     #test of the sphero bot
+    time.sleep(2)
 
